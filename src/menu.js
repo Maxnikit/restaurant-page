e0928f7feced6c59d34e8675e9af8b24c93cebb5
Maxnikit <max.nikit.03@gmail.com>
2023-12-13 19:31:38 +0300

    improve prices

--- src/menu.js
+++ src/menu.js
@@ -12,35 +12,35 @@ export function loadMenu() {
     const foodArray = [
       {
         name: "French Bowl",
-        price: 2.99,
+        price: 200,
         image: "../src/assets/frenchBowl.png",
         description:
           "Indulge in a bowl brimming with golden, crispy french fries, seasoned to perfection for a savory delight.",
       },
       {
         name: "French Burger",
-        price: 3.99,
+        price: 400,
         image: "../src/assets/frenchBurger.png",
         description:
           "A delicious twist on the classic burger - crispy french fries tucked inside a juicy patty, delivering the perfect crunch with every bite.",
       },
       {
         name: "Ice Cream",
-        price: 1.99,
+        price: 200,
         image: "../src/assets/iceCream.png",
         description:
           "Treat yourself to a delightful cone of creamy, sweet ice cream, the perfect cool and satisfying dessert.",
       },
       {
         name: "Coca Cola",
-        price: 1.99,
+        price: 100,
         image: "../src/assets/cola.png",
         description:
           "Quench your thirst with a refreshing cup of ice-cold Coca-Cola, the ideal companion to any meal.",
       },
       {
         name: "Birthday Cake",
-        price: 5.99,
+        price: 1000,
         image: "../src/assets/birthdayCake.png",
         description:
           "Celebrate in style with three colossal burgers adorned with festive candles, making every bite a party!",
@@ -50,12 +50,17 @@ export function loadMenu() {
       const card = document.createElement("div");
       const name = document.createElement("h2");
       const price = document.createElement("price");
+      const priceIcon = document.createElement("img");
+      const priceText = document.createElement("div");
       const image = document.createElement("img");
       const description = document.createElement("p");
 
       card.className = "menu-card";
       name.textContent = foodArray[i].name;
-      price.textContent = "$" + foodArray[i].price;
+      priceIcon.src = "../src/assets/icons8-v-bucks-48.png";
+      priceText.textContent = foodArray[i].price;
+      price.appendChild(priceIcon);
+      price.appendChild(priceText);
       image.src = foodArray[i].image;
       image.className = "menu-image";
       description.textContent = foodArray[i].description;
