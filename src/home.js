6ad2328569b65e6d28121b0a37fec5f5c1dac84b
Maxnikit <max.nikit.03@gmail.com>
2023-12-14 21:45:58 +0300

    test

--- src/home.js
+++ src/home.js
@@ -20,7 +20,7 @@ export function loadHome() {
   image.width = "400";
   description.id = "homeDescription";
   description.textContent =
-    "Welcome to Durrr Burger, where every visit is a flavor-packed journey! Our iconic burger chain, scattered across Fortnite's diverse landscapes, boasts more than just mouthwatering burgers; it's an immersive experience. With our juicy signature Durrr Burger stealing the show, accompanied by crispy fries and refreshing drinks, your taste buds are in for a treat. But Durrr Burger isn't just about the food—it's a cultural hub. Snap a pic with our friendly mascot, soak in the vibrant atmosphere, and create unforgettable moments in a place where great food and adventure collide. Join us at Durrr Burger for an unforgettable fusion of taste and fun!";
+    "elcome to Durrr Burger, where every visit is a flavor-packed journey! Our iconic burger chain, scattered across Fortnite's diverse landscapes, boasts more than just mouthwatering burgers; it's an immersive experience. With our juicy signature Durrr Burger stealing the show, accompanied by crispy fries and refreshing drinks, your taste buds are in for a treat. But Durrr Burger isn't just about the food—it's a cultural hub. Snap a pic with our friendly mascot, soak in the vibrant atmosphere, and create unforgettable moments in a place where great food and adventure collide. Join us at Durrr Burger for an unforgettable fusion of taste and fun!";
   hoursHeader.id = "hoursHeader";
   hoursHeader.textContent = "Available hours:";
   hours.id = "hours";
